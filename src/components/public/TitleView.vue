fdc8eeef35132da9a251973a490bb6bb9ee10f88
zeusocder <1309170466@qq.com>
2016-12-11 00:11:50 +0800

    标题栏组件

---
+++ src/components/public/TitleView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="titleView">
+    <em></em>
+    <span>{{ title }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .titleView {
+    padding-bottom: 15px;
+    border-bottom: 1px solid #CCC;
+  }
+  .titleView em {
+    display: inline-block;
+    background-color: #FF8D06;
+    width: 4px;
+    height: 25px;
+    margin-right: 10px;
+  }
+  .titleView span {
+    font-size: 24px;
+    position: relative;
+    bottom: 4px;
+  }
+</style>
