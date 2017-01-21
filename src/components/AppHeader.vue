fba452f9708eae071ddc533f2172146235e0d6ca
zeuscoder <1309170466@qq.com>
2017-01-21 17:39:03 +0800

    配置 autoprefixer 支持 ie9 css样式打包

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,7 +14,7 @@
     <!-- Header Navbar -->
     <nav class="navbar navbar-static-top" role="navigation">
       <!-- Sidebar toggle button-->
-      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+      <a class="sidebar-toggle" data-toggle="offcanvas" role="button">
         <span class="sr-only">Toggle navigation</span>
       </a>
       <!-- Navbar Right Menu -->
